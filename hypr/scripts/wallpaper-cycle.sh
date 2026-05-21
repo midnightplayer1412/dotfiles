4de6a93bb493cfc833397aabccad3c8496587334
midnightplayer1412 <johnpanda13@gmail.com>
2026-05-21 11:01:19 +0800

    fix(hypr): version updated patch fix

--- hypr/scripts/wallpaper-cycle.sh
+++ hypr/scripts/wallpaper-cycle.sh
@@ -9,7 +9,7 @@ while true; do
     wallpaper=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.webp" -o -name "*.gif" \) ! -name "lock.*" | shuf -n 1)
 
     if [ -n "$wallpaper" ]; then
-        swww img "$wallpaper" \
+        awww img "$wallpaper" \
             --transition-type "$TRANSITION_TYPE" \
             --transition-duration "$TRANSITION_DURATION"
 
