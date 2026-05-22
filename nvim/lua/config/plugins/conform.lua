67b25599135bed77afd98f6c2a94c7ea9bfa70dc
midnightplayer1412 <johnpanda13@gmail.com>
2026-05-22 11:42:40 +0800

    chore: misc dotfile updates
    - README.md describing the dotfile stack
    - hypr: external monitor rules by EDID description, minor hyprlock color tweak
    - tmux: prefix indicator in status bar, git branch + clock in status-right,
      reload binding, tmux-continuum + tmux-yank plugins
    - yazi: file manager config (init/keymap/yazi/package), matugen-driven theme template
    - nvim: conform.lua formatter config, lazy-lock refresh
    - gitignore yazi runtime theme and plugin manager output

---
+++ nvim/lua/config/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>fm",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+		},
+		format_on_save = {
+			timeout_ms = 3000,
+			lsp_fallback = true,
+		},
+	},
+}
---
+++ yazi/init.lua
@@ -0,0 +1,5 @@
+-- Yazi plugin init — keep small.
+
+require("git"):setup {
+	order = 1500,
+}
